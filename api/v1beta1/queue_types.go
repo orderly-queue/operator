a4527f96ed54d3f75d60b6317ea564afabaa2490
Henry Whitaker <henrywhitaker3@outlook.com>
2024-10-28 15:18:50 +0000

    feat: added operator init

---
+++ api/v1beta1/queue_types.go
@@ -0,0 +1,86 @@
+/*
+Copyright 2024.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1beta1
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+type ResourcesSpec struct {
+	// +required
+	Limit corev1.ResourceList `json:"limits,omitempty"`
+}
+
+type SnapshotSpec struct {
+	// +optional
+	// +default=false
+	Enabled bool `json:"enabled"`
+
+	// +optional
+	// +default="0 * * * *"
+	Schedule string `json:"schedule"`
+}
+
+// QueueSpec defines the desired state of Queue
+type QueueSpec struct {
+	// +required
+	Resources ResourcesSpec `json:"resources"`
+
+	// //+optional
+	// Snapshots SnapshotSpec `json:"snapshots"`
+}
+
+// QueueStatus defines the observed state of Queue
+type QueueStatus struct {
+	// +optional
+	// +operator-sdk:csv:customresourcedefinitions:type=status,xDescriptors={"urn:alm:descriptor:io.kubernetes.conditions"}
+	Conditions []metav1.Condition `json:"conditions,omitempty"`
+}
+
+func (q *QueueStatus) SetCondition(condition metav1.Condition) {
+	if q.Conditions == nil {
+		q.Conditions = make([]metav1.Condition, 0)
+	}
+	meta.SetStatusCondition(&q.Conditions, condition)
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// Queue is the Schema for the queues API
+type Queue struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   QueueSpec   `json:"spec,omitempty"`
+	Status QueueStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// QueueList contains a list of Queue
+type QueueList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Queue `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Queue{}, &QueueList{})
+}
